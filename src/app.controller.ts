50c0eafa2e3e15451e2d2eb80d8c3f5c8250422c
Thiwat Nuntjaruwong <thiwat@shopstack.asia>
2024-10-02 02:17:09 +0700

    Add public

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Public } from './common/decorators/public.decorator';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) { }
 
+  @Public()
   @Get('/health_check')
   healthCheck(): object {
     return this.appService.healthCheck();
